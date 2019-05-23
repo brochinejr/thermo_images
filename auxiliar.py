af7615860e13e5b8ee0e1f74c2af506445c6c6b7
brochinejr <carlosbrochinejr@gmail.com>
2019-05-22 21:33:19 -0300

    basic realease of auxiliar function

---
+++ auxiliar.py
@@ -0,0 +1,17 @@
+''' auxiliar_functions
+
+    Series of tools for using in project development.
+
+Created on 05/05/2019
+@author: Carlos Cesar Brochine Junior
+
+'''
+
+
+def funcao_test(teste):
+    return teste
+
+
+if __name__ == '__main__':
+    a = funcao_test('Hello World')
+    print(a)
